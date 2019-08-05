6f282e5e3dc65c04bc3af9230af2f069adeff24a
scOwez <owez@scalist.net>
2019-08-06 00:44:58 +0100

    Formatted docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 use std::collections::LinkedList;
 
 /// Template for an array in `owens-ml`.
-/// 
+///
 /// # Notes
-/// 
+///
 /// - It is advised against making an array containing arrays with `(a-a)`
 /// though it is not strictly forbidden inside of the parser.
-/// 
+///
 /// # Examples
-/// 
+///
 /// - `owens-ml`: data_type = `(s)`, used_data = `"hello!"`
-/// - Rust: data_type = `DataType::ArrayType.data_type`, 
+/// - Rust: data_type = `DataType::ArrayType.data_type`,
 /// used_data = `DataType::ArrayType.used_data`
 pub struct ArrayType {
     pub data_type: DataType,

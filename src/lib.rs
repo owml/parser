481b68612c501705b4a2a93883648ec460027252
scOwez <owez@scalist.net>
2019-08-06 23:57:39 +0100

    Basic, well-written datatypes parser made

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,55 @@
-use std::collections::LinkedList;
+#[macro_use]
+extern crate nom;
 
-/// Template for an array in `owens-ml`.
-///
-/// # Notes
-///
-/// - It is advised against making an array containing arrays with `(a-a)`
-/// though it is not strictly forbidden inside of the parser.
-///
-/// # Examples
-///
-/// - `owens-ml`: data_type = `(s)`, used_data = `"hello!"`
-/// - Rust: data_type = `DataType::ArrayType.data_type`,
-/// used_data = `DataType::ArrayType.used_data`
-pub struct ArrayType {
-    pub data_type: DataType,
-    pub used_data: DataType,
-}
+use nom::{bytes::complete::is_not, character::complete::char, sequence::delimited};
+use std::str;
 
 /// The main enum for identifying owens-ml datatypes. All written datatypes
-/// are lower-cased versions of these options; for example the `(s)` in
-/// owens-ml is `DataType::S` in this parser.
+/// are lower-cased versions of these options.
+///
+/// # Translated Types
+///
+/// - [DataType::StringType]: `(s)`
+/// - [DataType::IntType]: `(i)`
+/// - [DataType::ArrayType]: `(a-[type])` *where `[type]` is a type that's not an array*
+/// - [DataType::ObjectType]: `(o)`
+#[derive(Debug)]
 pub enum DataType {
-    S(String),
-    I(i32),
-    A(Box<ArrayType>),
-    O(LinkedList<DataType>),
+    StringType,
+    IntType,
+    ArrayType,
+    ObjectType,
+}
+
+/// The main error enum for `owens-ml`.
+///
+/// # Error types
+///
+/// - [SyntaxError::InvalidDataType]: When the datatype is not found (does not
+/// line up to any datatype inside of the [DataType] struct)
+/// - [SyntaxError::DataTypeNotFound]: When there are an empty set of empty
+/// parenthesis (no datatype given)
+pub enum SyntaxError {
+    InvalidDataType,
+    DataTypeNotFound,
+}
+
+named!(
+    get_dt<DataType>,
+    map_res!(delimited(char('('), is_not(")"), char(')')), build_dt)
+);
+
+fn build_dt(input: &[u8]) -> Result<DataType, SyntaxError> {
+    let input_stringified = match str::from_utf8(input) {
+        Ok(x) => x,
+        Err(_) => return Err(SyntaxError::DataTypeNotFound),
+    };
+
+    match input_stringified {
+        "s" => Ok(DataType::StringType),
+        "i" => Ok(DataType::IntType),
+        "o" => Ok(DataType::ObjectType),
+        "a" => Ok(DataType::ArrayType),
+        _ => Err(SyntaxError::InvalidDataType),
+    }
 }
